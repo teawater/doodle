04699ebed3f847c66718bca4a6ade04e06926299
teawater <teawater@126.com>
2017-04-30 09:22:20 +0800

    tmp change

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,13 +2,26 @@ var ctx_xy = {
   x: 0,
   y: 0,
   l: 0,
-}
+};
+var res;
 Page({
   data: ctx_xy,
   canvasIdErrorCallback: function (e) {
     console.error(e.detail.errMsg)
   },
   onReady: function (e) {
+    try {
+  res = wx.getSystemInfoSync()
+  console.log(res.model)
+  //console.log(res.pixelRatio)
+  console.log(res.windowWidth)
+  console.log(res.windowHeight)
+  console.log(res.language)
+  console.log(res.version)
+  console.log(res.platform)
+} catch (e) {
+  // Do something when catch error
+}
     var ctx = wx.createCanvasContext('firstCanvas')
 
     ctx.setFillStyle('red')
@@ -17,8 +30,8 @@ Page({
   },
   ctx_touchstart: function (e) {
     this.setData({
-      x: e.changedTouches[0].x,
-      y: e.changedTouches[0].y,
+      x: e.changedTouches[0].x/(res.windowWidth/750),
+      y: e.changedTouches[0].y/(res.windowWidth/750),
       l: e.changedTouches.length
     })
   }
