da12fbfceed36e10b41c96de961f566a0395b050
teawater <teawater@126.com>
2017-05-02 23:18:01 +0800

    aupdate

--- common.js
+++ common.js
@@ -1,14 +1,33 @@
 // common.js
 var px_rpx_ration
+var point_size
 
 function setup_px_rpx_ratio() {
   var info = wx.getSystemInfoSync()
+
   px_rpx_ration = info.windowWidth / 750
+  point_size = Math.ceil(px_rpx_ration) * 2
 }
 
 function px2rpx(px) {
-  return px / px_rpx_ration
+  return Math.round(px / px_rpx_ration)
+}
+
+function rpx2px(rpx) {
+  return Math.round(rpx * px_rpx_ration)
+}
+
+function px2draw(px) {
+  return Math.floor(px / point_size) * point_size
+}
+
+function fillRect(ctx, x, y) {
+  x = px2draw(x)
+  y = px2draw(y)
+  ctx.fillRect(x, y, point_size, point_size)
 }
 
 module.exports.setup_px_rpx_ratio = setup_px_rpx_ratio
 module.exports.px2rpx = px2rpx
+module.exports.rpx2px = rpx2px
+module.exports.fillRect = fillRect
--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -2,35 +2,18 @@ var common = require('../../common.js')
 
 var ctx
 
-var ctx_xy = {
-  x: 0,
-  y: 0,
-  l: 0,
-  str: "",
-}
-
 Page({
-  data: ctx_xy,
-  canvasIdErrorCallback: function (e) {
-    console.error(e.detail.errMsg)
-  },
   onReady: function (e) {
     ctx = wx.createCanvasContext('firstCanvas')
 
     ctx.setFillStyle("#ffffff")
-    ctx.fillRect(0, 0, 749, 749)
-    ctx.setFillStyle("#000000")
-    ctx.fillRect(10, 10, 1, 1)
-    ctx.draw()
+    ctx.fillRect(0, 0, common.rpx2px(750), common.rpx2px(750))
+    ctx.draw(true)
   },
   ctx_touch: function (e) {
-    //console.log(e)
-    var new_str = e.type+parseInt(common.px2rpx(e.changedTouches[0].x)).toString()+" "+parseInt(common.px2rpx(e.changedTouches[0].y)).toString()+ "\n" + this.data.str
-    this.setData({
-      // x: common.px2rpx(e.changedTouches[0].x),
-      // y: common.px2rpx(e.changedTouches[0].y),
-      // l: e.changedTouches.length
-      str: new_str
-    })
+    ctx.setFillStyle("#000000")
+    ctx.setFillStyle("#ff0000")
+    common.fillRect(ctx, e.changedTouches[0].x, e.changedTouches[0].y)
+    ctx.draw(true)
   }
 })
