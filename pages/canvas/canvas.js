ff5840abb9fb900337f289441cff2a6893321168
teawater <teawater@126.com>
2017-05-14 23:14:54 +0800

    Update

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -1,9 +1,40 @@
 var common = require('../../common.js')
 
+const wss_url = 'wss://127.0.0.1/'
 var ctx
 
+function socketopen(res) {
+  console.log('WebSocket连接已打开！')
+  this.setData({
+    status: '服务器连接成功，下载数据，请稍等...',
+  })
+  wx.sendSocketMessage({
+    data:"0"
+  })
+}
+
 Page({
-  onReady: function (e) {
+  data: {
+    status: '努力连接服务器中，请稍等...',
+    show_status: true
+  },
+  onLoad: function(options) {
+    wx.connectSocket({
+      url: wss_url,
+    })
+    wx.onSocketOpen(socketopen)
+/*     wx.onSocketOpen(function(res){
+  
+  wx.sendSocketMessage({
+      data:"Canvas"
+    })
+}) */
+    wx.onSocketError(function(res){
+      console.log('WebSocket连接打开失败，请检查！')
+    })
+  },
+
+  onReady: function () {
     ctx = wx.createCanvasContext('firstCanvas')
 
     ctx.setFillStyle("#ffffff")
