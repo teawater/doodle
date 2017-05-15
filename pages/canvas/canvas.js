989c427a6df1c69a098d970c6bc2122c736faef7
teawater <teawater@126.com>
2017-05-15 22:05:29 +0800

    Update

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -3,7 +3,7 @@ var common = require('../../common.js')
 const wss_url = 'wss://127.0.0.1/'
 var ctx
 
-function socketopen(res) {
+/* function socketopen(res) {
   console.log('WebSocket连接已打开！')
   this.setData({
     status: '服务器连接成功，下载数据，请稍等...',
@@ -11,26 +11,38 @@ function socketopen(res) {
   wx.sendSocketMessage({
     data:"0"
   })
-}
+} */
 
 Page({
   data: {
     status: '努力连接服务器中，请稍等...',
-    show_status: true
   },
+
   onLoad: function(options) {
     wx.connectSocket({
       url: wss_url,
     })
-    wx.onSocketOpen(socketopen)
-/*     wx.onSocketOpen(function(res){
-  
-  wx.sendSocketMessage({
-      data:"Canvas"
-    })
-}) */
+    wx.onSocketOpen(this.socketopen)
     wx.onSocketError(function(res){
       console.log('WebSocket连接打开失败，请检查！')
+    })
+  },
+
+  socketopen: function(res) {
+    this.setData({
+      status: '服务器连接成功，下载数据，请稍等...',
+    })
+    wx.sendSocketMessage({
+      data:"0"
+    })
+  },
+
+  socketerror: function(res) {
+    this.setData({
+      status: '服务器连接出错，重新连接，请稍等...',
+    })
+    wx.sendSocketMessage({
+      data:"0"
     })
   },
 
